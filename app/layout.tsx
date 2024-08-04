ca6ba9e51efb3b3512076c3b17d7126fc459b302
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-08-04 06:22:10 +0530

    Prisma Clinet actions and form and DialogBox

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import NextTopLoader from "nextjs-toploader";
+import DesignerContextProvider from "@/components/context/DesignerContext";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <NextTopLoader />
+          <DesignerContextProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
+        </body>
+      </html>
+   
   );
 }
